c2c8256c3466e4a0e0dd993eec7b6187c9d54fd4
danteimerito <danteimerito@gmail.com>
2023-11-21 16:26:26 -0500

    edit landing page

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="contain-flex">
+    <div class="contain-no-padding">
         <div class="feature-card">
             <h1>Easily Identify Trees in Pennsylvania</h1>
             <button @click="toggleMenu">Begin the Journey</button>
