ecbd78021412f5929d0b53f25a2b8403678088e6
danteimerito <danteimerito@gmail.com>
2024-05-14 14:49:12 -0400

    Add developer credit to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,24 +6,28 @@
                 <router-link to="/privacypolicy" rel="noopener noreferrer">Privacy Policy</router-link>
                 <a href="mailto:patreeguide@gmail.com">Contact</a>
             </div>
+            <div>
+                <a href="https://www.danteimerito.com" target="_blank">Site by Dante</a>
+            </div>
         </div>
     </footer>
 </template>
 
 <script setup>
-    const d = new Date();
-    const year = d.getFullYear();
+const d = new Date();
+const year = d.getFullYear();
 </script>
 
 <style scoped>
-    footer a:hover {
-        color: chartreuse;
-    }
-    .below-copyright {
-        display: flex;
-        flex-direction: row;
-        gap: 15px;
+footer a:hover {
+    color: chartreuse;
+}
+
+.below-copyright {
+    display: flex;
+    flex-direction: row;
+    gap: 15px;
 
 
-    }
+}
 </style>
