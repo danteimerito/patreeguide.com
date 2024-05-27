df4fded9bfd5175e3dae8c601e4bdc62b73215d2
danteimerito <danteimerito@gmail.com>
2024-05-26 20:53:26 -0400

    Remove development api server configuration

--- vue.config.js
+++ vue.config.js
@@ -4,19 +4,19 @@ module.exports = defineConfig({
   transpileDependencies: true
 })
 
-module.exports = {
-  transpileDependencies: ['vuex-persist'],
-  devServer: {
-    proxy: {
-      '^/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        logLevel: 'debug',
-        pathRewrite: { '^/api': '/' },
-      },
-    },
-  },
-}
+// module.exports = {
+//   transpileDependencies: ['vuex-persist'],
+//   devServer: {
+//     proxy: {
+//       '^/api': {
+//         target: 'http://localhost:5000',
+//         changeOrigin: true,
+//         logLevel: 'debug',
+//         pathRewrite: { '^/api': '/' },
+//       },
+//     },
+//   },
+// }
 
 
 
